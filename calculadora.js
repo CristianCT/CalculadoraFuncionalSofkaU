73e5e87edc099620be693d77f6acaf66ef2982af
Cristian Cabarcas <cridejecato@gmail.com>
2021-12-09 17:49:16 -0500

    Función dividir

--- calculadora.js
+++ calculadora.js
@@ -10,12 +10,21 @@ function range(start, end){
     return (new Array(end - start + 1)).fill(undefined).map((_, k) =>k + start);
 }
 
+
 function multiplicar(a, b){
+    if(a === 0 || b === 0) return 0;
     return range(0, b)
-            .reduce((previous, current) => previous + a)
+        .reduce((previous, current) => sumar(previous, a))
 }
 
 function dividir(num, den){
+    if(den === 0) return null;
+    if(num === 0) return 0;
+    return range(1, num)
+        .reduce((previous, current) => sumar(previous, multiplicar(current, den) <= num? 1: 0));
 }
 
-console.log(dividir(90,9));
+console.log(sumar(99, 1));
+console.log(restar(55, 25));
+console.log(multiplicar(5, 8));
+console.log(dividir(99, 3));
